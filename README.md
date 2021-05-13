c4cf5d098839212adf73a92badcc62ba651318dc
leek <zealotzergling@gmail.com>
2021-05-13 02:44:10 -0400

    Init
    I'm hungry

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Freemotes
  "Free" Discord Nitro with extra steps
+ ~~Basically a gif sender~~
+##### All gif emotes are 49x49 to mimic real emotes at standard Discord size
+
+How it works...
+
+1. Reads and saves all csv data into a dictionary
+**Make sure there are no spaces in csv and follow formatting (using commas) or else it won't work properly**
+
+2. Detects keypresses and if in a specific pattern, types out a gif link.
+	Ex:
+	> input: ":pepelaugh:" + any key press after
+	> output: a link associated with the key :pepelaugh:
+
+## Available Functions
+
+On Tab Press:
+- Autocompletes emote based on closest search
+ 	Ex:
+ 	> input: ":pepel" + tab key press
+ 	> output: ":pepelaugh:"
+
---
+++ freemote.py
@@ -0,0 +1,82 @@
+from types import TracebackType
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
+import pyautogui
+import csv
+import time
+
+# Version 1.5
+
+emote = [""]
+
+cx = 0
+
+emote_dict = {}
+
+# Loads Emotes
+with open("freemotes.csv", "r") as f:
+    rd = csv.reader(f)
+
+    for row in rd:
+        emote_dict[row[0]] = row[1]
+        print(f"Emote Loaded: {row[0]}")
+
+    f.close()
+# Main func
+def on_press(key):
+    global emote, cx
+    
+    if key == Key.shift_r or key == Key.shift_l:
+        return
+    
+    if len(emote) < 1:
+        emote = [""]
+        return
+    
+    if emote[0] == ":":
+        if key == Key.backspace:
+            emote.pop()
+            return
+    
+    if str(key) == "':'":
+        if len(emote) == 1:
+            emote.clear()
+            emote.append(":")
+        else:
+            emote.append(":")
+    elif emote[0] == ":" and emote[-1] == ":" and len(emote) > 2:
+        jl = "".join(emote).lower()
+        emote = [""]
+        print(f"Emote Detected: {jl}")
+        pyautogui.hotkey("ctrl", "a")
+        pyautogui.press("backspace")
+        try:
+            send = emote_dict[jl]
+            pyautogui.typewrite(send)
+            print(f"Emote Sent: {jl}")
+        except:
+            print(f"{jl} is not a valid emote")
+            pass
+    elif emote[0] == ":":
+        try:
+            if key.char == ";":
+                emote.append(":")
+            else:
+                emote.append(key.char)
+        except:
+            if key == Key.backspace:
+                emote.pop()
+            elif key == Key.tab:
+                cur = "".join(emote)
+                try:
+                    fd = [x for x in emote_dict.keys() if x.startswith(cur)][0]
+                except:
+                    print(f"No emote starts with {cur}")
+                    return
+                rd = fd[len(cur):len(fd)][:-1]
+                pyautogui.typewrite(rd)
+                pyautogui.typewrite(":")
+
+# stuff i dont understand
+with Listener(on_press=on_press,on_release=None) as listener:
+    listener.join()
